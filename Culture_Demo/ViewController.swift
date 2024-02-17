e499aa02162149c253bc9306cdb8fa6b3f88ef57
taeuk178 <nous178@naver.com>
2024-02-18 02:19:13 +0900

    Initial Commit

---
+++ Culture_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Culture_Demo
+//
+//  Created by tw on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
